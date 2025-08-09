682bc88d7290699a2ff31de0e7a69bd484cc6fb7
James Carl Etol <78995050+T3rr8us-P4nk@users.noreply.github.com>
2025-08-09 14:53:15 +0800

    Update script.js

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,1 +1,10 @@
+// practice 1 - print information
+function submit() {
+    const name = 'Hi my name is ';
+    const food = '. I like eating ';
 
+    const nameInput = document.getElementById("nameInput").value;
+    const foodInput = document.getElementById("foodInput").value;
+
+    document.getElementById("display").textContent = name + nameInput + food + foodInput + '.';
+}
